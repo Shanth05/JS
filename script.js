a579c87e160be51e35e6fc0385b2c32ca7ff48ad
Pirashanth <shanth.pirashanth5@gmail.com>
2024-02-28 23:46:39 +0530

    Example

---
+++ script.js
@@ -0,0 +1,31 @@
+
+
+/* Author: S.Pirashanth
+Date : 26.0.2024
+Title : Java Script Bootcamp
+ 
+console.log("Welcome to Java Script"); //This is demo code
+window.alert("Hello Student");
+
+// Variables
+
+let x=10;
+let y=10.23;
+let z="shanth";
+let v=true;
+
+console.log(typeof x);
+console.log(typeof y);
+console.log(typeof z);
+console.log(typeof v);
+
+//Arithmetic
+
+*/
+
+let username;
+document.getElementById("submit").onclick=
+function () {
+    username = document.getElementById("text").value;
+    console.log(username);
+}
